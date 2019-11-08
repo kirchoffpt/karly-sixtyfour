283a9a4aa5bfb941d9662b8a38d447a24f4dd342
ptk <kirchoffpt@gmail.com>
2019-11-07 23:19:47 -0500

    Revert "indents to 2 spaces"
    This reverts commit 948781606311b055df4db16903c2628e7e2f2de0.

--- src/chess_funcs.cpp
+++ src/chess_funcs.cpp
@@ -5,152 +5,152 @@ using namespace std;
 
 void print_bitboard(U64 board)
 {
-  int i,j,k;
+	int i,j,k;
 
-  cout << endl;
+	cout << endl;
 
-  for(i=0,j=0;i<64;i++){
-    if(j==0) cout << endl;
-    if ((board & (0x8000000000000000))==0x8000000000000000){
-      cout << 'x';
-    } else {
-      cout << '-';
-    }
-    board <<= 1;
-    j = (j+1)%8;
-  }
+	for(i=0,j=0;i<64;i++){
+		if(j==0) cout << endl;
+		if ((board & (0x8000000000000000))==0x8000000000000000){
+			cout << 'x';
+		} else {
+			cout << '-';
+		}
+		board <<= 1;
+		j = (j+1)%8;
+	}
 
-  cout << endl;
+	cout << endl;
 
-  return;
+	return;
 }
 
 void print_bitboard(U64 board, ofstream& ofs)
 {
-  int i,j,k;
+	int i,j,k;
 
-  ofs << endl;
+	ofs << endl;
 
-  for(i=0,j=0;i<64;i++){
-    if(j==0) ofs << endl;
-    if ((board & (0x8000000000000000))==0x8000000000000000){
-      ofs << 'x';
-    } else {
-      ofs << '-';
-    }
-    board <<= 1;
-    j = (j+1)%8;
-  }
+	for(i=0,j=0;i<64;i++){
+		if(j==0) ofs << endl;
+		if ((board & (0x8000000000000000))==0x8000000000000000){
+			ofs << 'x';
+		} else {
+			ofs << '-';
+		}
+		board <<= 1;
+		j = (j+1)%8;
+	}
 
-  ofs << endl;
+	ofs << endl;
 
-  return;
+	return;
 }
 
 bool is_sliding_piece(int piece_type)
 {
-  if ((piece_type == BISHOP)||(piece_type == ROOK)||(piece_type == QUEEN)) return true;
-  return false;
+	if ((piece_type == BISHOP)||(piece_type == ROOK)||(piece_type == QUEEN)) return true;
+	return false;
 }
 
 string piece_itos(int piece_type)
 {
-  switch(piece_type){
-    case PAWN   : return "PAWN";
-      break;
-    case KNIGHT : return "KNIGHT";
-      break;
-    case BISHOP : return "BISHOP";
-      break;
-    case ROOK : return "ROOK";
-      break;
-    case QUEEN  : return "QUEEN";
-      break;
-    case KING   : return "KING";
-  }
-  return "UNDEFINED_PIECE_TYPE";
+	switch(piece_type){
+		case PAWN 	:	return "PAWN";
+			break;
+		case KNIGHT :	return "KNIGHT";
+			break;
+		case BISHOP	:	return "BISHOP";
+			break;
+		case ROOK	:	return "ROOK";
+			break;
+		case QUEEN	:	return "QUEEN";
+			break;
+		case KING 	:	return "KING";
+	}
+	return "UNDEFINED_PIECE_TYPE";
 }
 
 int bit_to_idx(U64 bitboard)
 {
-  unsigned long idx;
+	unsigned long idx;
 
-  _BitScanForward64( &idx, bitboard);
+	_BitScanForward64( &idx, bitboard);
 
-  return int(idx);
+	return int(idx);
 }
 
 string idx_to_coord(int idx)
 {
-  int x = idx%8;
-  int y = idx/8;
-  char letters[8] = {'a','b','c','d','e','f','g','h'};
-  char buffer[32];
-  string coord = "";
+	int x = idx%8;
+	int y = idx/8;
+	char letters[8] = {'a','b','c','d','e','f','g','h'};
+	char buffer[32];
+	string coord = "";
 
-  y = y + 1;
+	y = y + 1;
 
-  coord.append(sizeof(char),(letters[7-x]));
-  coord.append(itoa(y,buffer,10));
+	coord.append(sizeof(char),(letters[7-x]));
+	coord.append(itoa(y,buffer,10));
 
-  return coord;
+	return coord;
 }
 
 void print_move(unsigned short move)
 {
-  cout << move_itos(move);
+	cout << move_itos(move);
 }
 
 string move_itos(unsigned short move)
 {
-  unsigned short x;
-  string s = "";
-  s += idx_to_coord((move & SRC_MASK) >> SRC_SHIFT);
-  s += idx_to_coord((move & DST_MASK) >> DST_SHIFT);
-  x = move & SPECIAL_MASK;
-  if(x == PROMO){
-    //" nbrq"[move & PROMO_MASK];
-    s += "nbrq"[move & PROMO_MASK];
-  }
-  return s;
+	unsigned short x;
+	string s = "";
+	s += idx_to_coord((move & SRC_MASK) >> SRC_SHIFT);
+	s += idx_to_coord((move & DST_MASK) >> DST_SHIFT);
+	x = move & SPECIAL_MASK;
+	if(x == PROMO){
+		//" nbrq"[move & PROMO_MASK];
+		s += "nbrq"[move & PROMO_MASK];
+	}
+	return s;
 }
 
 int get_ray_dir(int a, int b) //a,b indexes
 {
-  int x, y;
-  
-  x = a%8 - b%8;
-  y = b/8 - a/8;
+	int x, y;
+	
+	x = a%8 - b%8;
+	y = b/8 - a/8;
 
-  if(b > a){ //W, NW, N, NE
-    if(x < 0 ){
-      if(y == 0){
-        return W_DIR;
-      } else {
-        return NW_DIR;
-      }
-    } else { 
-      if(x == 0){
-        return N_DIR;
-      } else{
-        return NE_DIR;
-      }
-    }
-  } else { // E, SE, S, SW 
-    if(x > 0){
-      if(y == 0){
-        return E_DIR;
-      } else {
-        return SE_DIR;
-      }
-    } else {
-      if(x == 0){
-        return S_DIR;
-      } else{
-        return SW_DIR;
-      }
-    }
-  }
-  return -1;
+	if(b > a){ //W, NW, N, NE
+		if(x < 0 ){
+			if(y == 0){
+				return W_DIR;
+			} else {
+				return NW_DIR;
+			}
+		} else { 
+			if(x == 0){
+				return N_DIR;
+			} else{
+				return NE_DIR;
+			}
+		}
+	} else { // E, SE, S, SW 
+		if(x > 0){
+			if(y == 0){
+				return E_DIR;
+			} else {
+				return SE_DIR;
+			}
+		} else {
+			if(x == 0){
+				return S_DIR;
+			} else{
+				return SW_DIR;
+			}
+		}
+	}
+	return -1;
 }
 
--- src/uciengine.cpp
+++ src/uciengine.cpp
@@ -13,78 +13,78 @@
 using namespace std;
 
 void uci_position(istringstream& is, chess_pos* rootpos){
-  string token, s, fenstring = STARTPOS;
-  char move_string[5];
-  unsigned short move = 0;
+	string token, s, fenstring = STARTPOS;
+	char move_string[5];
+	unsigned short move = 0;
 
-  is >> token;
-  if(token == "startpos"){
-    fenstring = STARTPOS;
-  } else if(token == "fen"){
-    is >> token;
-    while(token.back() != QUOTATION_MARK && is){
-      is >> s;
-      token.append(" "+s);
-    }
-    fenstring = token;
-  } else {
-    return;
-  }
-  rootpos->load_new_fen(fenstring);
-  rootpos->generate_moves();
-  rootpos->sort_piece_list();
-  //cout << token << endl;
-  if(is >> token && token == "moves"){
-    while(is >> token){
-      strcpy(move_string, token.c_str());
-      move = rootpos->pos_move_list.get_move_from_string(move_string);
-      if(move != 0){
-        rootpos->add_move(move);
-        rootpos->generate_moves();
-        rootpos->sort_piece_list();
-      }
-    }
-  }
-  //rootpos->sort_piece_list();
-  //rootpos->print_pos(true);
+	is >> token;
+	if(token == "startpos"){
+		fenstring = STARTPOS;
+	} else if(token == "fen"){
+		is >> token;
+		while(token.back() != QUOTATION_MARK && is){
+			is >> s;
+			token.append(" "+s);
+		}
+		fenstring = token;
+	} else {
+		return;
+	}
+	rootpos->load_new_fen(fenstring);
+	rootpos->generate_moves();
+	rootpos->sort_piece_list();
+	//cout << token << endl;
+	if(is >> token && token == "moves"){
+		while(is >> token){
+			strcpy(move_string, token.c_str());
+			move = rootpos->pos_move_list.get_move_from_string(move_string);
+			if(move != 0){
+				rootpos->add_move(move);
+				rootpos->generate_moves();
+				rootpos->sort_piece_list();
+			}
+		}
+	}
+	//rootpos->sort_piece_list();
+	//rootpos->print_pos(true);
 }
 
 int main(int argc, char *argv[]){
-  string cmd, token;
-  chess_pos *rootpos = new chess_pos(STARTPOS);
-  search_handler *searcher = new search_handler(rootpos);
-  time_t system_time = chrono::system_clock::to_time_t(chrono::system_clock::now());
+	string cmd, token;
+	chess_pos *rootpos = new chess_pos(STARTPOS);
+	search_handler *searcher = new search_handler(rootpos);
+	time_t system_time = chrono::system_clock::to_time_t(chrono::system_clock::now());
 
-  rootpos->generate_moves();
+	rootpos->generate_moves();
 
 
-  ofstream ofs (FILEOUT, ofstream::app);
-  ofs << endl;
-  ofs << "////////////////////////////////////////////" << endl;
-  ofs << ctime(&system_time);
-  ofs << "////////////////////////////////////////////" << endl;
+	ofstream ofs (FILEOUT, ofstream::app);
+	ofs << endl;
+	ofs << "////////////////////////////////////////////" << endl;
+	ofs << ctime(&system_time);
+	ofs << "////////////////////////////////////////////" << endl;
 
-  while(getline(cin,cmd)){
-    istringstream is(cmd);
-    ofs << cmd << endl;
-    is >> token;
-    if(token == "uci"){
-      cout << "id name karly64\n";
-      cout << "id author Paul Kirchoff\n";
-      cout << "uciok\n";
-    } else if(token == "isready"){
-      cout << "readyok\n";
-    } else if(token == "position"){
-      uci_position(is, rootpos);
-    } else if(token == "go"){
-      searcher->search();
-    } else if(token == "stop"){
-      searcher->stop();
-    } else if(token == "quit"){
-      break;
-    }
-  }
+	while(getline(cin,cmd)){
+		istringstream is(cmd);
+		ofs << cmd << endl;
+		is >> token;
+		if(token == "uci"){
+			cout << "id name karly64\n";
+			cout << "id author Paul Kirchoff\n";
+			cout << "uciok\n";
+		} else if(token == "isready"){
+			cout << "readyok\n";
+		} else if(token == "position"){
+			uci_position(is, rootpos);
+		} else if(token == "go"){
+			searcher->search();
+		} else if(token == "stop"){
+			searcher->stop();
+		} else if(token == "quit"){
+			break;
+		}
+	}
 
-  ofs.close();
-  return 0;
+	ofs.close();
+	return 0;
 }
