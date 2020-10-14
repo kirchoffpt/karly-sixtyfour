46ead4de050564e8ef491bd73cb5f4ff500d4927
P K <57437864+kirchoffpt@users.noreply.github.com>
2020-10-14 18:54:03 -0400

    tiny change to linux bitscan

--- src/bitops.cpp
+++ src/bitops.cpp
@@ -37,12 +37,8 @@ unsigned char bitops::bscanr64(unsigned long* index, U64 mask)
     #ifdef __linux__
         if(mask == 0) return 0;
         unsigned long lz = __builtin_clzll(mask);
-        if(lz){
-            *index = 63-lz;
-            return 1;
-        } else {
-            return 0;
-        }
+        *index = 63-lz;
+        return 1;
     #elif _WIN32
         return _BitScanReverse64(index, mask);
     #else
