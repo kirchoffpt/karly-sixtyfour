44d29da48aaa943cedfd94431e4c440503c4abf7
ptkubuntu <kirchoffpt@gmail.com>
2020-10-28 08:10:02 -0400

    added googletest install to cmake

--- test/chess_funcs_test.cpp
+++ test/chess_funcs_test.cpp
@@ -2,26 +2,10 @@
 #include "chess_funcs.h"
 #include "gtest/gtest.h"
 
-TEST(FactorialTest, Negative) {
-  // This test is named "Negative", and belongs to the "FactorialTest"
-  // test case.
+TEST(ChessFuncs, IdxToCoord) {
+  EXPECT_EQ(idx_to_coord(0), "h1");
   EXPECT_EQ(idx_to_coord(1), "g1");
-
-
-  // <TechnicalDetails>
-  //
-  // EXPECT_EQ(expected, actual) is the same as
-  //
-  //   EXPECT_TRUE((expected) == (actual))
-  //
-  // except that it will print both the expected value and the actual
-  // value when the assertion fails.  This is very helpful for
-  // debugging.  Therefore in this case EXPECT_EQ is preferred.
-  //
-  // On the other hand, EXPECT_TRUE accepts any Boolean expression,
-  // and is thus more general.
-  //
-  // </TechnicalDetails>
+  EXPECT_EQ(idx_to_coord(63), "a8");
 }
 
 
